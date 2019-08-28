aa8e995fc5cee1d9d3d9b48d827631bcd1d88a0e
Milos <savkevip@gmail.com>
2019-08-28 19:13:49 +0200

    added advices

--- HomePage.js
+++ HomePage.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { users } from '../data/users';
 
-// const korisnici = [
+// const users = [
 //     { id: 1, name: "Milos", active: true },
 //     { id: 2, name: "Vuk", active: true },
 //     { id: 3, name: "Banos", active: true },
@@ -14,59 +14,67 @@ import { users } from '../data/users';
 //     { id: 10, name: "Djedo", active: false },
 //   ];
 
-const  korisnici = users ;
-console.log(korisnici)
-  
+
+// todo -> navikavaj se da koristis engleski
 
 
-export default class HomePage extends React.Component{
-    constructor(){
-        super();
-            this.state = {
-            pojam: '',
-            podaci: korisnici,
-            pronadjeniPodaci: []
-        };
-           
-    };
-
-    handleChange = (event) => {
-        this.setState({pojam: event.target.value})
+export default class HomePage extends Component {
+    // constructor ti ne treba iskreno kad vec korstis arrow funkcije
+    // searchValue ti je vredost koju uzimas iz inputa u Searchu
+    state = {
+        searchValue: '',
+        currentUser: null,
+        customers: users
     }
-    //ovu funkciju bih pozvao na dugme, da se na taj nacin trazi korisnik ili ne?
-    //tek zapoceto
+
+    // ovo je dobro
+    handleChange = event => {
+        this.setState({ searchValue: event.target.value })
+    }
+    
     findUser = () => {
-        if(this.state.pojam === this.podaci.map(korisnik => korisnik.name)){
-            return pronadjeniPodaci
-        }
+        // imamo vredost u inputu searchValue
+        // imam i customers listu (nazvao sam je customers eto onako) u kojoj to treba da nadjemo - customera po imenu
+        const filttredCustomers = this.state.customers.filter(customer => {
+           // znaci filtriramo listu  
+           if (customer.name.includes(this.state.searchValue)) {
+            return customer;
+           }
+           // ovim gore ces da vratis korisnike u cijem imenu se sadrzi vrednost iz searchValue-a
+           // includes je built in metoda u JS-u -> vraca true / false
+           // filter i map uvek vracaju listu da znas - u ovom slucaju tu filtriranu novu listu sam stavio u promenljivu...
+           // i postavio je na mesto customers...
+           // i onda ce u renderu da se izrenderuje nova ta lista
+           // setState uvek okida render metodu u komponenti...
+           this.setState({
+            customers: filttredCustomers
+           });
+        });
     }
-    render(){
-     
-         return(
+
+    render() {
+         return (
             <div>
-                <p>{this.state.pojam}</p>
                 <label>Search box</label>
                 <input
-                type="text"
-                placeholder="Search"
-                value={this.state.value}
-                onChange={this.handleChange}
-
+                    type="text"
+                    placeholder="Search"
+                    value={this.state.searchValue}
+                    onChange={this.handleChange}
                 />
-    
-
-                    {
-                     this.state.podaci.map(korisnik => 
-                             (
-                                <div key={korisnik.id}>
-                                <h1>{korisnik.name}</h1>
-                                </div>
-                            )
+                // ovde mozes da dodas npr da ne moze da se klikne - stavis button umesto spana... pa disabled itd...
+                // findUser ukoliko searchValue je ''
+                // msm ono kao nema potrebe svakako ce da vrati celu listu
+                <span onClick={this.findUser}>FIND</span>
+                <div>
+                    {this.state.customers.map(customer => (
+                            <div key={customer.id}>
+                                <h1>{customer.name}</h1>
+                            </div>
                         )
-                    }
-                 </div>
-                //ovde gore sam vezbao map()
-                
+                    )}
+                </div>
+            </div>
         );
     }
 }
